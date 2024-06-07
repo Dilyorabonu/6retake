f16b6472ac08aa03259bc2d779d2c3f5009caf0f
dilyorabonu <dilyorabonu@gmail.com>
2024-06-07 09:41:40 +0500

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,62 @@
-import { createBrowserRouter , RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
-import Home from './pages/Home'
-import Contact  from "./pages/Contact";
-import About  from "./pages/About";
-import Error  from './pages/Error'
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Error from "./pages/Error";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 function App() {
+  const [count, setCount] = useState(0);
+  <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   const routes = createBrowserRouter([
     {
-      path:"/",
-      element:<MainLayout/>,
-      children:[
+      path: "/",
+      element: <MainLayout />,
+      children: [
         {
-          index:true,
-          element:<Home/>,
+          index: true,
+          element: <Home />,
         },
         {
-          path:"/about",
-          element:<About/>
+          path: "/about",
+          element: <About />,
         },
         {
-          path:"/contact",
-          element:<Contact/>
+          path: "/contact",
+          element: <Contact />,
         },
         {
-          path:"*",
-          element:<Error/>
-        }
-      ]
+          path: "*",
+          element: <Error />,
+        },
+      ],
     },
-  ])
-  return <RouterProvider router={routes} />
+  ]);
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
